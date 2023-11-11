83a3652de8100fe36e35085dd4c2cbffabd879d1
Sanket8887 <72690873+Sanket8887@users.noreply.github.com>
2023-11-11 10:27:39 +0530

    All files

---
+++ script.js
@@ -0,0 +1,28 @@
+var i = 0;
+var txt = 'Hi there! I am a final year IT student with expertise in programming and web development. I am passionate about creating innovative solutions and love tackling coding challenges. I am eager to bring my skills to a dynamic team and contribute to exciting projects.'; 
+var speed = 40; 
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
